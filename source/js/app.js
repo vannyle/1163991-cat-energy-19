37800fdb88b4f4b6b7703359ed832254cde8c931
vanny <vannnyle@gmail.com>
2020-04-24 11:49:54 +0300

    Change burger menu from svg to css. Add animation for menu.

--- source/js/app.js
+++ source/js/app.js
@@ -1,21 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
-  const navMain = document.querySelector('.nav');
-  const navToggle = document.querySelector('.toggle');
+  const navMain = document.querySelector('.page-header__nav');
+  const navToggle = document.querySelector('.page-header__toggle');
 
-  navToggle.classList.remove('toggle__nojs');
-  navMain.classList.remove('nav__nojs');
+  navToggle.classList.remove('page-header__toggle--nojs');
+  navMain.classList.remove('page-header__nav--nojs');
 
   navToggle.addEventListener('click', function () {
-    if (navMain.classList.contains('nav__close')) {
-      navMain.classList.remove('nav__close');
-      navMain.classList.add('nav__open');
-      navToggle.classList.remove('toggle__close');
-      navToggle.classList.add('toggle__open')
+    if (navMain.classList.contains('page-header__nav--close')) {
+      navMain.classList.remove('page-header__nav--close');
+      navMain.classList.add('page-header__nav--open');
+      navToggle.classList.remove('page-header__toggle--close');
+      navToggle.classList.add('page-header__toggle--open')
     } else {
-      navMain.classList.remove('nav__open');
-      navMain.classList.add('nav__close');
-      navToggle.classList.remove('toggle__open');
-      navToggle.classList.add('toggle__close')
+      navMain.classList.remove('page-header__nav--open');
+      navMain.classList.add('page-header__nav--close');
+      navToggle.classList.remove('page-header__toggle--open');
+      navToggle.classList.add('page-header__toggle--close')
     }
   })
 });
